2ad45fe485c4f5ec6e2f4731e0e530450e73241e
Alexandros Kontos <alexiskon01@gmail.com>
2020-12-31 23:28:18 +0200

    update user

--- src/app/feutured/accountsettings/accountsettings.component.ts
+++ src/app/feutured/accountsettings/accountsettings.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/shared/user';
+import { UpdateuserService } from '../services/updateuser.service';
+import { UserprofileService } from '../services/userprofile.service';
 
 @Component({
   selector: 'app-accountsettings',
@@ -7,9 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountsettingsComponent implements OnInit {
 
-  constructor() { }
+  updateForm: FormGroup;
+  submitted: boolean = false;
+  matchedPassword: boolean = false;
+  user: User;
+
+  constructor(private fb: FormBuilder, private update: UpdateuserService,
+    private router: Router, private profile: UserprofileService) { }
 
   ngOnInit(): void {
-  }
 
+    this.profile.getUserProfile().subscribe(data => {
+      console.log(data.body.name)
+      this.updateForm = this.fb.group({
+        email: [data.body.email],
+        name: [data.body.name],
+        password: [null, Validators.minLength(8)],
+        confirmPassword: [null, Validators.minLength(8)]
+      })
+    })
+  }
+  submitUpdate() {
+    this.submitted = true;
+    if (this.updateForm.controls.confirmPassword.value === this.updateForm.controls.password.value) {
+      this.matchedPassword = true;
+    }
+    if (this.updateForm.invalid || this.matchedPassword === false) {
+      return;
+    } else {
+      //email already exists error handle
+      let credentials: User = this.updateForm.value
+      console.log(credentials)
+      this.update.updateUser(credentials).subscribe(value => {
+        localStorage.setItem('userid', value._id)
+        localStorage.setItem('token', value.token)
+        this.router.navigate(['home'])
+      })
+    }
+  }
 }
--- src/app/feutured/services/updateuser.service.ts
+++ src/app/feutured/services/updateuser.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UpdateuserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  url = 'http://localhost:3000/users/me'
+  updateUser(credentials): Observable<any> {
+    let token = localStorage.getItem('token')
+    return this.http.patch<any>(this.url, credentials, {  headers: new HttpHeaders().set('Authorization', token) })
+  }
 }
